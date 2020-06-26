42879388062a083e9cf47707853b6c9180e29fac
hanu <hanu.sas@gmail.com>
2020-06-26 14:43:24 +0800

    Fixes based on design

--- pkg/metrics-server/server_test.go
+++ pkg/metrics-server/server_test.go
@@ -7,7 +7,6 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	storage "sigs.k8s.io/metrics-server/pkg/storage"
 )
 
 func TestServer(t *testing.T) {
@@ -15,39 +14,22 @@ func TestServer(t *testing.T) {
 	RunSpecs(t, "Metrics server Suite")
 }
 
+
 var _ = Describe("Metrics server", func() {
 	var (
 		server *MetricsServer
-		s      *storage.Storage
 		req    *http.Request
 	)
 
 	BeforeEach(func() {
-		s = storage.NewStorage()
-		server = &MetricsServer{resolution: 60 * time.Second, storage: s}
+		server = &MetricsServer{resolution: 60 * time.Second}
 	})
 
-	It("liveness should fail when last scrape failed", func() {
+	It("liveness should fail when last scrape didnt happen on the resolution ticker ", func() {
 		By("last scrape was 2 minutes ago")
 		server.lastTickStart = time.Now().Add(-2 * time.Minute)
 		err := server.CheckLiveness(req)
 		Expect(err).ToNot(BeNil())
 		Expect(err.Error()).Should(ContainSubstring("was greater than expected metrics resolution"))
 	})
-
-	It("liveness should fail when storage is empty", func() {
-		By("last scrape was succcesful")
-		server.lastTickStart = time.Now().Add(1 * time.Minute)
-		err := server.CheckLiveness(req)
-		Expect(err).ToNot(BeNil())
-		Expect(err.Error()).Should(ContainSubstring("no metrics available in storage cache"))
-	})
-
-	It("readines error when storage is empty", func() {
-		By("Checking for readiness")
-		err := server.CheckReadiness(req)
-		Expect(err).ToNot(BeNil())
-		Expect(err.Error()).Should(ContainSubstring("no metrics available in storage cache"))
-	})
-
 })
